5c5754075d170209836ceb5d08e28f0512b4f595
Stephen Punak <steve@punak.com>
2024-09-03 17:15:57 -0700

    Changes for diff testing

--- subdir/testclass1.h
+++ subdir/testclass1.h
@@ -1,15 +1,31 @@
 #ifndef TESTCLASS1_H
 #define TESTCLASS1_H
 
+#include <QString>
+
+
 
 class TestClass1
 {
 public:
     TestClass1();
 
-
 private:
     int _value;
+
+    QString _x1;
+    QString _x2;
+    QString _x3;
+    QString _t4;
+    QString _t5;
+
+    int _v1;
+    int _v2;
+    int _v3;
+
+    QString _s1;
+    QString _s2;
+    QString _s3;
 };
 
 #endif // TESTCLASS1_H
